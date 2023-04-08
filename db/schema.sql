89ecb8dd9e6516b282ceb8ab1a402ce6f2622094
Samantha Gosselin <samanthag2009@gmail.com>
2023-04-08 16:45:31 -0500

    debugged schema and seeds, now seeding properly in mysql

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
----Create and use new database--
+/*Create and use new database*/
 
 DROP DATABASE IF EXISTS employee_tracker; 
 
@@ -6,28 +6,28 @@ CREATE DATABASE employee_tracker;
 
 USE employee_tracker;
 
---Create Department Table--
+/*Create Department Table*/
   
 CREATE TABLE department (
-    id: INT NOT NULL PRIMARY KEY NOT NULL,
-    name: VARCHAR(30) NOT NULL
-)
+    id INT NOT NULL PRIMARY KEY NOT NULL,
+    department_name VARCHAR(30) NOT NULL
+);
 
---Create Role Table--
+/*Create Role Table*/
 
 CREATE TABLE role (
-    id: INT NOT NULL PRIMARY KEY NOT NULL,
-    title: VARCHAR(30) NOT NULL,
-    salary: DECIMAL NOT NULL,
-    department_id: INT NOT NULL
-)
+    id INT NOT NULL PRIMARY KEY NOT NULL,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL
+);
 
---Create Employee Table--
+/*Create Employee Table*/
 
 CREATE TABLE employee (
-    id: INT NOT NULL PRIMARY KEY NOT NULL,
-    first_name: VARCHAR(30) NOT NULL,
-    last_name: VARCHAR(30) NOT NULL,
-    role_id: INT NOT NULL,
-    manager_id: INT
-)
+    id INT NOT NULL PRIMARY KEY NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    manager_id INT
+);
