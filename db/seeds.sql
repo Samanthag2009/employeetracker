ecf440256743568b6a639a6ba9c80e4145034415
Samantha Gosselin <samanthag2009@gmail.com>
2023-04-08 20:07:06 -0500

    added functionality for department-related start menu items and messed up my seeds. TO DO: fix ID generation

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,27 +1,27 @@
 /* Create Insert Information for Department id and name */
 
-INSERT INTO department (id, department_name) 
-VALUES (111, 'Magic Spells'),
-       (222, 'Evil Plans'),
-       (333, 'World Domination'),
-       (444, 'Forbidden Knowledge'),
-       (555, 'Human Resources');
+INSERT INTO department (department_name) 
+VALUES ('Magic Spells'),
+       ('Evil Plans'),
+       ('World Domination'),
+       ('Forbidden Knowledge'),
+       ('Human Resources');
 
 
 /* Create Insert Information for Role id, title, salary and department_id */
 
-INSERT INTO role (id, title, salary, department_id)
-VALUES (666, 'Mastermind', 500000, 222),
-       (777, 'Wizard', 100000, 111),
-       (888, 'Politician', 900000, 333),
-       (999, 'Lich', 700000, 444),
-       (000, 'Human Resources Employee', 60000, 555);
+INSERT INTO role (title, salary, department_id)
+VALUES ('Mastermind', 500000, 1),
+       ('Wizard', 100000, 2),
+       ('Politician', 900000, 3),
+       ('Lich', 700000, 4),
+       ('Human Resources Employee', 60000, 5);
 
 /* Create Insert Information for Employee id, first_name, last_name, role_id and manager_id */
 
-INSERT INTO employee (id, first_name, last_name, role_id, manager_id)
-VALUES (1, 'Agoth', 'the Terrible', 000, 1),
-       (2, 'John', 'Rogers', 777, 2),
-       (3, 'Kevin', 'Brown', 999, 3),
-       (4, 'Lindsey', 'Jenkins', 666, 4),
-       (5, 'Arcadia', 'Zelfor', 888, 5);
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ('Agoth', 'the Terrible', 4, 111),
+       ('John', 'Rogers', 8, 222),
+       ('Kevin', 'Brown', 5, 333),
+       ('Lindsey', 'Jenkins', 2, 444),
+       ('Arcadia', 'Zelfor', 1, 555);
